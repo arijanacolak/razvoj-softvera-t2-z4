a8356aa689d180c39f9cf03a7156049e2cf84090
arijanacolak <acolak1@etf.unsa.ba>
2020-03-19 16:19:28 +0100

    Postavljene boje i raspored

--- src/ba/unsa/etf/rs/tutorijal2/Main.java
+++ src/ba/unsa/etf/rs/tutorijal2/Main.java
@@ -12,7 +12,7 @@ public class Main extends Application {
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("/fxml/sample.fxml"));
         primaryStage.setTitle("Ispit test");
-        primaryStage.setScene(new Scene(root, 300, 275));
+        primaryStage.setScene(new Scene(root, 611, 485));
         primaryStage.show();
     }
 
